88dafa0ba27cb8c2864afc041de7782c24301264
zavx0z <metaversebdfl@gmail.com>
2023-06-05 12:01:03 +0300

    remove other screens

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -3,8 +3,6 @@
 import 'package:flutter/material.dart';
 
 import 'pages/counter.dart';
-import 'pages/dash.dart';
-import 'pages/text.dart';
 
 import 'src/js_interop.dart';
 import 'package:url_strategy/url_strategy.dart';
@@ -23,9 +21,7 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   final ValueNotifier<int> _counter = ValueNotifier<int>(0);
-  final ValueNotifier<DemoScreen> _screen = ValueNotifier<DemoScreen>(DemoScreen.counter);
   late final AppStateManager _state;
-  final ValueNotifier<String> _text = ValueNotifier<String>('');
 
   @override
   void dispose() {
@@ -35,26 +31,11 @@ class _MyAppState extends State<MyApp> {
   @override
   void initState() {
     super.initState();
-    _state = AppStateManager(
-      screen: _screen,
-      counter: _counter,
-      text: _text,
-    );
+    _state = AppStateManager( counter: _counter );
     final export = createDartExport(_state);
 
     // Выпустите это через корневой объект приложения Flutter :)
     broadcastAppEvent('flutter-initialized', export);
-  }
-
-  Widget demoScreenRouter(DemoScreen which) {
-    switch (which) {
-      case DemoScreen.counter:
-        return CounterDemo(counter: _counter);
-      case DemoScreen.text:
-        return TextFieldDemo(text: _text);
-      case DemoScreen.dash:
-        return DashDemo(text: _text);
-    }
   }
 
   @override
@@ -65,10 +46,9 @@ class _MyAppState extends State<MyApp> {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
-      home: ValueListenableBuilder<DemoScreen>(
-        valueListenable: _screen,
-        builder: (context, value, child) => demoScreenRouter(value),
-      ),
+      routes: {
+        '/': (context) => CounterDemo(counter: _counter),
+      },
     );
   }
 }
--- app/lib/src/js_interop/counter_state_manager.dart
+++ app/lib/src/js_interop/counter_state_manager.dart
@@ -1,18 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:js/js.dart';
 
-enum DemoScreen {
-  counter('counter'),
-  text('text'),
-  dash('dash');
-
-  const DemoScreen(String screen) : _screen = screen;
-  final String _screen;
-
-  @override
-  String toString() => _screen;
-}
-
 /// This is the bit of state that JS is able to see.
 ///
 /// It contains getters/setters/operations and a mechanism to
@@ -20,17 +8,9 @@ enum DemoScreen {
 @JSExport()
 class AppStateManager {
   // Creates a DemoAppStateManager wrapping a ValueNotifier.
-  AppStateManager({
-    required ValueNotifier<DemoScreen> screen,
-    required ValueNotifier<int> counter,
-    required ValueNotifier<String> text,
-  })  : _counter = counter,
-        _text = text,
-        _screen = screen;
+  AppStateManager({ required ValueNotifier<int> counter }) : _counter = counter;
 
-  final ValueNotifier<DemoScreen> _screen;
   final ValueNotifier<int> _counter;
-  final ValueNotifier<String> _text;
 
   // _counter
   int getClicks() {
@@ -49,34 +29,8 @@ class AppStateManager {
     _counter.value--;
   }
 
-  // _text
-  void setText(String text) {
-    _text.value = text;
-  }
-
-  String getText() {
-    return _text.value;
-  }
-
-  // _screen
-  void setScreen(String screen) {
-    _screen.value = DemoScreen.values.byName(screen);
-  }
-
-  String getScreen() {
-    return _screen.value.toString();
-  }
-
   // Allows clients to subscribe to changes to the wrapped value.
   void onClicksChanged(VoidCallback f) {
     _counter.addListener(f);
-  }
-
-  void onTextChanged(VoidCallback f) {
-    _text.addListener(f);
-  }
-
-  void onScreenChanged(VoidCallback f) {
-    _screen.addListener(f);
   }
 }
